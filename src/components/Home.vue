614bfeff90398cde2453c11854c68d8fc9a5d835
waadene-wi <laili8202914@163.com>
2021-04-13 19:09:10 +0800

    首页的制作完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,115 +1,84 @@
 <template>
 	<div id="tmpl">
 	 <!-- 1.0 轮播图：mint-ui中的 swipe组件实现 -->
-		 <mt-swipe :auto="2000">
+		 <mt-swipe :auto="4000">
 		  <!-- <mt-swipe-item>1</mt-swipe-item>
 		   -->
 		   <mt-swipe-item v-for="item in list">
-		   	<img :src="item.img">
-		   </mt-swipe-item>
+		   <img :src="item.img"></mt-swipe-item>
+		   
 		</mt-swipe>
 
 		<!-- 2.0 利用MUI的9宫格样式实现导航区域 -->
 		<div class="mui-content">
 		        <ul class="mui-table-view mui-grid-view mui-grid-9">
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-		            <router-link to="/news/newslist">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="#/news/newslist">
 		                    <span class="mui-icon mui-icon-home"></span>
-		                    <div class="mui-media-body">新闻资讯</div></router-link>
-		            </li>
-
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/photo/photolist">
-		                    <span class="mui-icon mui-icon-email">
-
-		                    </span>
-		                    <div class="mui-media-body">图片分享</div></router-link>
-		             </li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/goods/goodslist">
+		                    <div class="mui-media-body">新闻资讯</div></router-link></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="#">
+		                    <span class="mui-icon mui-icon-email"></span>
+		                    <div class="mui-media-body">图片分享</div></router-link></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="#">
 		                    <span class="mui-icon mui-icon-chatbubble"></span>
-		                    <div class="mui-media-body">商品购买</div></router-link>
-
-		            </li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/feedback">
+		                    <div class="mui-media-body">商品购买</div></router-link></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="#">
 		                    <span class="mui-icon mui-icon-location"></span>
-		                    <div class="mui-media-body">留言反馈</div></router-link>
-
-		            </li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/vide">
+		                    <div class="mui-media-body">留言反馈</div></router-link></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="#">
 		                    <span class="mui-icon mui-icon-search"></span>
-		                    <div class="mui-media-body">视频专区</div></router-link>
-
-		             </li>
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="/callme">
+		                    <div class="mui-media-body">视频专区</div></router-link></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><router-link to="#">
 		                    <span class="mui-icon mui-icon-phone"></span>
-		                    <div class="mui-media-body">联系我们</div></router-link>
-		            </li>
-		            
+		                    <div class="mui-media-body">联系我们</div></router-link></li>
+		           
 		        </ul> 
 		</div>
+	
 
 	</div>
 </template>
 
 <script>
+
 import { Toast } from 'mint-ui';
 import common from '../kits/common.js';
-
-	export default{
-		data(){
-		return {
+export default{
+	data(){
+		return{
 			list:[]
-			}
-		},
-		created(){
-			// 当页面中的data和methods对象都创建完毕以后，就会自动调用created
-			this.getimgs();
-		},
-		methods:{
-			getimgs(){
-				// 实现轮播组件中的数据请求
-				// 1.0 确定url
-				var url = common.apidomain + '/api/getlunbo';
-
-				// 2.0 调用$http.get()
-				this.$http.get(url).then(function(response){
-					var data = response.body;
-					//错误处理
-					if(data.status != 0){
-						Toast(data.message);
-						return;
-					}
-
-					// 如果服务器返回的数据正常则赋值给list
-					this.list = data.message;
-				});
-			}
+		}
+	},
+	created(){
+		// this.getData()
+	},
+	methods:{
+		getData(){
+			var url = common.apidomain + '/api/getlunbo';
+			this.$http.get(url).then(function(res){
+				var data = res.body;
+				if (data.status!=0) {
+					Toast(data.message);
+					return;
+				}
+				this.list = data.message;
+			})
 		}
 	}
+}
+
 </script>
 
 <style scoped>
 	.mint-swipe{
-		height: 300px;
-	}
-	.mint-swipe-item img{
-		width: 100%;
-	}
-	.mint-swipe-item{
+		height: 200px;
 		background-color: red;
-		width: 100%;
-		height: 300px;
 	}
-	
-	.mui-content,.mui-content ul{
-		background-color: #fff;
+	.mui-content .mui-grid-view{
+		background-color: white;
+		border:0;
 	}
-	.mui-grid-view.mui-grid-9 .mui-table-view-cell{
-		border-right:0px;
-		border-bottom:0px; 
-	}
-	.mui-grid-view.mui-grid-9{
-		border-top:0px;
-		border-left:0px; 
+	.mui-content .mui-grid-view .mui-table-view-cell{
+			border:0;
 	}
 	.mui-icon-home:before,
 	.mui-icon-email:before,
@@ -143,4 +112,5 @@ import common from '../kits/common.js';
 	.mui-icon-phone:before{		
 		background-image: url(../../statics/imgs/6.png);		
 	}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -9,23 +9,18 @@ import vueRouter from 'vue-router';
 // 3.0.1 将vueRouter对象绑定到Vue对象上
 Vue.use(vueRouter);
 
-// 3.0.2 导入路由规则对应的组件对象
 import home from './components/Home.vue';
-import shopcar from './components/shopcar/car.vue';
-import newslist from './components/news/newslist.vue';
-import newsinfo from './components/news/newsinfo.vue';
+import newlist from './components/news/newslist.vue';
 
 
-// 3.0.2 定义路由规则
 var router1 = new vueRouter({
-	linkActiveClass:'mui-active',  //改变路由激活时的class名称
+	linkActiveClass:'mui-active', 
 	routes:[
-		{path:'/home',component:home},   //首页
-		{path:'/shopcar',component:shopcar},  // 购物车
-		{path:'/news/newslist',component:newslist}, //新闻资讯列表页面
-		{path:'/news/newsinfo/:id',component:newsinfo}  // 新闻资讯详情页面
-	]
-	});
+		{path:"/home",component:home},
+		{path:"/news/newslist",component:newlist},
+	],
+});
+
 
 
 // 4.0 注册mint-ui
@@ -42,16 +37,10 @@ import '../statics/mui/css/mui.css';
 // 6.0 导入一个当前系统的全局基本样式
 import '../statics/css/site.css';
 
-// 7.0 将vue-resource在vue中绑定，自动在vue对象实例上注入一个$http对象就可以使用ajax方法了
 import vueResource from 'vue-resource';
 Vue.use(vueResource);
 
-// 8.0 定义一个全局过滤器实现日期的格式化
-import moment from 'moment';
-Vue.filter('datefmt',function(input,fmtstring){
-//	使用momentjs这个日期格式化类库实现日的格式化功能
-	return moment(input).format(fmtstring);
-});
+
 
 // 5.0 利用Vue对象进行解析渲染
 new Vue({
